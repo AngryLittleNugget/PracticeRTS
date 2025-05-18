895ad0c4a5776d1c4c901aded090992d859eac3d
AngryLittleNugget <keenanmwalter@gmail.com>
2025-05-18 10:36:49 -0600

    Push 1: Finished first video

---
+++ Assets/Scripts/HexMetrics.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public static class HexMetrics
+{
+    public static float HexRadius {get; private set;}
+    public static float OuterRadius (float hexSize){
+        return hexSize;
+    }
+    public static float InnerRadius (float hexSize){
+        return hexSize * (Mathf.Sqrt(3)/2);
+    }
+
+    public static Vector3[] Corners(float hexSize, HexOrientation orientation){     
+        Vector3[] corners= new Vector3[6];
+        for (int i=0; i <6; i++){
+            corners[i] = Corner(hexSize, orientation, i);
+        }
+        return corners;
+    }
+
+    public static Vector3 Corner(float hexSize, HexOrientation orientation, int index){
+
+        float angle = 60f *index;
+        if (orientation == HexOrientation.Pointy){
+            angle += 30f;
+        }
+        Vector3 corner = new Vector3(hexSize * Mathf.Cos(angle * Mathf.Deg2Rad),
+            0f,
+            hexSize * Mathf.Sin(angle * Mathf.Deg2Rad)
+            );
+        return corner;
+    }
+
+    public static Vector3 Center(float hexSize, int x, int z, HexOrientation orientation){
+        Vector3 centerPosition;
+        if (orientation == HexOrientation.Pointy){
+            centerPosition.x = (x + z * 0.5f - z / 2) * (InnerRadius(hexSize) *2f);
+            centerPosition.y = 0f;
+            centerPosition.z = z * (OuterRadius(hexSize) * 1.5f);
+        }
+      else{ // FlatTop: Horizontal rows, columns stagger vertically.
+        centerPosition.x = x * (OuterRadius(hexSize) * 1.5f);
+        centerPosition.y = 0f;
+        centerPosition.z = (z + (x % 2 == 1 ? 0.5f : 0f)) * (InnerRadius(hexSize) * 2f);
+        }
+        return centerPosition;
+    }
+}
